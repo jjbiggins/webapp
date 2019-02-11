05c9615f23ef370a27ce1cf10364787be0035114
Joseph Biggins <joseph-biggins@uiowa.edu>
2019-02-10 22:22:03 -0600

    good work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {IndexComponent} from './index/index.component';
+import {ResumeComponent} from './resume/resume.component';
 
 
 const routes: Routes = [
+  {
+    path: 'resume',
+    component: ResumeComponent,
+  },
   {
     path: '',
     component: IndexComponent,
