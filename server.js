8085d782f46c3294a01362c66999e3653f94a158
jjbiggins <joseph-biggins@uiowa.edu>
2019-02-14 22:57:52 -0500

    Deploy

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.get('/*', function(req,res) {
 });
 
 // Start the app by listening on the default Heroku port
-app.listen('port', process.env.PORT || 8080);
+app.listen('port', process.env.PORT || 80);
