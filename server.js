8907b285cacd624e1720e9bcd1f60b279228645b
jjbiggins <joseph-biggins@uiowa.edu>
2019-03-19 21:52:25 -0500

    new run/debug config

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directoryng
-app.use(express.static(__dirname + '/dist/webapp'));
+app.use(express.static(__dirname + '/dist/joe-biggins'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/webapp/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/joe-biggins/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
