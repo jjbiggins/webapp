eb399d4ee4e1376db427e5ed13db42b3a5e9ccba
jjbiggins <joseph-biggins@uiowa.edu>
2019-03-21 17:44:30 -0500

    reset angular.json

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directoryng
-app.use(express.static(__dirname + '/dist/joe-biggins'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/webapp'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/joe-biggins/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/webapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
